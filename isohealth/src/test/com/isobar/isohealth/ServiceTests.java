e06bd9a555e8659f8be2377d88f33a07806426e4
dcohen <David.Cohen@isobar.com>
2011-10-05 18:39:08 -0400

    Added to ServiceTests suite

--- isohealth/src/test/com/isobar/isohealth/ServiceTests.java
+++ isohealth/src/test/com/isobar/isohealth/ServiceTests.java
@@ -8,11 +8,20 @@ public class ServiceTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite(ServiceTests.class.getName());
 		//$JUnit-BEGIN$
-		suite.addTestSuite(UserServiceTest.class);
-		suite.addTestSuite(SettingsServiceTest.class);
-		suite.addTestSuite(ProfileServiceTest.class);
+		suite.addTestSuite(BackgroundActivityServiceTest.class);
+		suite.addTestSuite(DiabetesMeasurementServiceTest.class);
 		suite.addTestSuite(FitnessActivityServiceTest.class);
+		suite.addTestSuite(GeneralMeasurementServiceTest.class);
+		suite.addTestSuite(NutritionMeasurementServiceTest.class);
+		suite.addTestSuite(PersonalRecordServiceTest.class);
+		suite.addTestSuite(ProfileServiceTest.class);
+		suite.addTestSuite(SettingsServiceTest.class);
+		suite.addTestSuite(SleepMeasurementServiceTest.class);
+		suite.addTestSuite(StreetTeamMembershipServiceTest.class);
 		suite.addTestSuite(StrengthTrainingActivityServiceTest.class);
+		suite.addTestSuite(UserServiceTest.class);
+		suite.addTestSuite(WeightMeasurementServiceTest.class);
+
 		//$JUnit-END$
 		return suite;
 	}
